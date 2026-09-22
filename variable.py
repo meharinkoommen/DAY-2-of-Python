76364bb89149b6fa5e30858c06041ee95a6d887f
meharinkoommen <meharinkoommen@gmail.com>
2026-09-22 17:41:55 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,47 @@
+
+'''
+
+---variable---
+---> A Variable is a container which can store the data or values
+---> The data may be
+1.Integer (int)
+2.String
+3.float
+4.Boolean(bool)
+
+type(a)---> is used to find out the data type of the value 
+type(a)---> is a method
+
+'''
+a=10
+b="MS Dhoni"
+#string willbe representeing by using " "
+c=34.5
+is_student=True
+
+'''
+
+Limitation of variable:
+1. Variable name cannot start with number but it will include insde or at the end of the variable.
+
+eg:     clg2=25            |       clg2university=21
+        print(clg2)        |       print(clg2university)
+output: 25                 |output:21
+
+2. No space is allowed in between two words
+
+eg:    first_name="Meharin" |        firstname="Meharin"
+       print(first_name)    |        print(firstname)
+output: Meharin             |output: Meharin
+
+3. No special character are allowed for the variable (start,middle,end:we cannot used)
+like; @,#,& etc...
+
+4. No python reserved keywords are allowed
+eg: 'if' we cannot used but we can use 'IF'
+
+'''
+
+
+
+
---
+++ variable2.py
@@ -0,0 +1,60 @@
+a=10
+a=20
+a=30
+print(a)
+
+
+a=10
+a=20
+print(a)
+a=30
+
+#variable value dynamically changed
+
+x=10,20,30
+print(x)
+
+'''
+
+        m,n,p=10
+        print(m,n,p)
+output: Error
+
+---> A single variable can hold the multiple values
+---> A multiple variable cannot hold the single value
+# valid variables
+
+
+Write a program about the student data:
+studentname
+studentage
+studentbranch
+login
+
+Print all the details and find out the datatype of each and every variable
+
+'''
+student_name="Meharin"
+print(student_name)
+
+student_age=18
+print(student_age)
+
+student_branch="AIML"
+print(student_branch)
+
+is_login=True
+print(is_login)
+
+
+print(student_name)
+print(type(student_name))
+
+print(student_age)
+print(type(student_age))
+
+print(student_branch)
+print(type(student_branch))
+
+print(is_login)
+print(type(is_login))
